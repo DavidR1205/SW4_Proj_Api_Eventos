6b8c2002d697880f74aa166d790bde5ca9bc1081
sebasg71 <jhoan.gomez@correo.tdea.edu.co>
2025-05-21 12:15:06 -0500

    Implementar vistas para la gestión de boletas: agregar, listar y editar

--- controllers/boletaController.js
+++ controllers/boletaController.js
@@ -4,10 +4,11 @@ const boletaModel = require('../models/boletaModel');
 exports.listarBoletas = async(req, res) => {
     try {
         const boletas = await boletaModel.obtenerBoletas();
-        res.json(boletas);
-        /*res.render('boletas/index', {
+        res.render(boletas);
+        res.render('boletas/index', {
+            title: 'Boletas',
             boletas
-        })*/
+        })
     } catch (error) {
         console.error(error);
         res.status(500).render('error', {
@@ -55,63 +56,55 @@ exports.editarBoleta = async (req, res) => {
     try {
         const boleta = await boletaModel.obtenerBoletaId(req.params.id);
         if (!boleta) {
-            return res.status(404).json({message: 'Boleta no encontrada'})/*render('error', {
+            return res.status(404).render('error', {
                 title: 'Error',
                 message: 'Boleta no encontrada'
-            });*/
+            });
         }
-        res.status(200).json(boleta)
-        /*res.render('boletas/form', {
+        res.render('boletas/form', {
             title: 'Editar Boleta',
             boleta,
             errors: [],
             isEditing: true
-        });*/
+        });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error al cargar los datos de la boleta'})
-        /*res.status(500).render('error', {
+        res.status(500).render('error', {
             title: 'Error',
             message: 'Error al cargar los datos de la boleta'
-        });*/
+        });
     }
 };
 
 exports.actualizarBoleta = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(400).json({
-            message: 'Error en la validacion',
-            errors: errors.array()
-        })
-        /*return res.render('boleta/form', {
+        return res.render('boleta/form', {
             title: 'Editar Boleta',
             boleta: { ...req.body, id: req.params.id},
             errors: errors.array(),
             isEditing: true
-        });*/
+        });
     }
 
     try {
         const success = await boletaModel.actualizarBoleta(req.params.id, req.body);
         if (!success) {
-            return res.status(404).json({message: 'Boleta no encontrada'});
-            /*return res.status(404).render('error', {
+            return res.status(404).render('error', {
                 title: 'Error',
                 message: 'Boleta no encontrada'
-            });*/
+            });
+            
         }
-        res.status(200).json({ message: 'Boleta actualizada con exito' });
-        //res.redirect('/boletas')
+        res.redirect('/boletas')
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error al actualizar la boleta' });
-        /*res.render('boletas/form', {
+        res.render('boletas/form', {
             title: 'Editar Boleta',
             boleta: {...req.body, id: req.params.id},
             errors: [{ message: 'Error al actualizar la boleta' }],
             idEditing: true
-        });*/
+        });
     }
 };
 
