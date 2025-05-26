43250a1fac6d6afdc9c3f828e17113d75df0d603
sebasg71 <jhoan.gomez@correo.tdea.edu.co>
2025-05-26 09:48:30 -0500

    se editaron las rutas para crear ventas y usuario

--- routes/usuarioRouter.js
+++ routes/usuarioRouter.js
@@ -4,6 +4,7 @@ const usuarioController = require('../controllers/usuarioController');
 const usuarioValidator = require('../middlewares/usuarioValidator');
 
 router.get('/', usuarioController.listarUsuarios); // Listar todos los usuarios
+router.get('/create', usuarioControllerController.formUsuario); // Formulario para crear un nuevo usuario
 router.post('/', usuarioValidator, usuarioController.agregarUsuario); // Crear un nuevo usuario
 router.get('/:id', usuarioController.editarUsuario); // Obtener un usuario por ID
 router.put('/:id', usuarioValidator, usuarioController.actualizarUsuario); // Actualizar un usuario existente
--- routes/ventaRouter.js
+++ routes/ventaRouter.js
@@ -5,6 +5,7 @@ const ventaValidator = require('../middlewares/ventaValidator');
 
 // Rutas para las ventas
 router.get('/', ventaController.listarVentas); // Listar todas las ventas
+router.get('/create', ventaController.formVenta); // Formulario para crear una nueva venta
 router.post('/', ventaValidator, ventaController.agregarVenta); // Crear una nueva venta
 router.get('/:id', ventaController.editarVenta); // Obtener una venta por ID
 router.put('/:id', ventaValidator, ventaController.actualizarVenta); // Actualizar una venta existente
